6b3da07b6350c29f9fde0d60c37bc6d3fe92c6b0
jyates <jasonyates07@googlemail.com>
2023-01-23 10:16:05 +0000

    Fixes #23 - API filtering does not work
    Adding missing filtersets to API

--- netbox_documents/api/views.py
+++ netbox_documents/api/views.py
@@ -1,21 +1,24 @@
 from netbox.api.viewsets import NetBoxModelViewSet
 
-from .. import models
+from .. import models, filtersets
 from .serializers import SiteDocumentSerializer, DeviceDocumentSerializer, DeviceTypeDocumentSerializer, CircuitDocumentSerializer 
 
 class SiteDocumentViewSet(NetBoxModelViewSet):
     queryset = models.SiteDocument.objects.prefetch_related('tags')
     serializer_class = SiteDocumentSerializer
+    filterset_class = filtersets.SiteDocumentFilterSet
 
 class DeviceDocumentViewSet(NetBoxModelViewSet):
     queryset = models.DeviceDocument.objects.prefetch_related('tags')
     serializer_class = DeviceDocumentSerializer
-
+    filterset_class = filtersets.DeviceDocumentFilterSet
 
 class DeviceTypeDocumentViewSet(NetBoxModelViewSet):
     queryset = models.DeviceTypeDocument.objects.prefetch_related('tags')
     serializer_class = DeviceTypeDocumentSerializer
+    filterset_class = filtersets.DeviceTypeDocumentFilterSet
 
 class CircuitDocumentViewSet(NetBoxModelViewSet):
     queryset = models.CircuitDocument.objects.prefetch_related('tags')
     serializer_class = CircuitDocumentSerializer
+    filterset_class = filtersets.CircuitDocumentFilterSet
